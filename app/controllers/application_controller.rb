6b3873a36a72dcff81bc982c9ecfc5705db9d51d
eli colussi <elicolussi@elis-MacBook-Pro.local>
2023-06-01 01:43:01 -0700

    added check if user was logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,5 +28,11 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
